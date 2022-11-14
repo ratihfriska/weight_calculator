9920f77b4bd947ca8116686d308011b05698d415
ratihfriska <71802999+ratihfriska@users.noreply.github.com>
2022-11-14 21:51:07 +0700

    Update calculator.dart

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -9,26 +9,14 @@ class Calculator {
   final int age;
   final Gender gender;
 
-  double _bmi = 0.0;
+  double _idealWeight = 0.0;
 
-  String calculateBMI() {
-    _bmi = (height-100) - ((height-100) * 10/100);
-    return _bmi.toStringAsFixed(1);
+  String calculateIdealWeight() {
+    _idealWeight = (height-100) - ((height-100) * 10/100);
+    return _idealWeight.toStringAsFixed(1);
   }
 
   String getResult() {
-    if (_bmi == _bmi) {
-      return 'Your Ideal Weight';
-    } else {
-      return 'Oh noo, yo dont have ideal weight';
+      return 'Your Ideal Weight Is';
     }
   }
-
-  String getInterpretation() {
-    if (_bmi == _bmi) {
-      return 'You have a normal body weight. Good job!';
-    } else {
-      return 'You have a lower than normal body weight. You can eat a bit more.';
-    }
-  }
-}
